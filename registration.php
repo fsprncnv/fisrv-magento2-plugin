99c63f6b80e683b8ab549fae291ef73361dcd525
pvijayakumar <princen.vijayakumar@cgi.com>
2024-08-28 16:53:01 +0200

    Update require files

--- registration.php
+++ registration.php
@@ -2,6 +2,14 @@
 
 declare(strict_types=1);
 
+if (file_exists(__DIR__ . '/magento-stubs-0.php')) {
+    require_once __DIR__ . '/magento-stubs-0.php';
+}
+
+if (file_exists(__DIR__ . '/magento-stubs-1.php')) {
+    require_once __DIR__ . '/magento-stubs-1.php';
+}
+
 // use Magento\Framework\Component\ComponentRegistrar;
 
 // ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Fisrv_Payment', __DIR__);
